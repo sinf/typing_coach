795296c1f7e4cbf9b90612e78d4fec312fc88521
ArhoM <arho.iivari@elisanet.fi>
2021-02-25 11:19:36 +0200

    handle special debug filename: stderr

--- src/debug.c
+++ src/debug.c
@@ -1,24 +1,28 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdarg.h>
 #include "debug.h"
 
-static FILE *debug_file=NULL;
+static FILE *the_debug_file=NULL;
 const char *debug_file_path=NULL;
 
 void debug_output_init(void)
 {
 	if (debug_file_path && debug_file_path[0]) {
-		debug_file = fopen(debug_file_path, "w");
+		if (strcmp(debug_file_path,"stderr")==0)
+			the_debug_file = stderr;
+		else
+			the_debug_file = fopen(debug_file_path, "w");
 	}
 }
 
 void debug_msg_x(const char *file, int line, const char *func, const char *fmt, ...)
 {
-	if (debug_file) {
+	if (the_debug_file) {
 		va_list a;
 		va_start(a, fmt);
-		fprintf(debug_file, "%s:%d:%s: ", file, line, func);
-		vfprintf(debug_file, fmt, a);
+		fprintf(the_debug_file, "%s:%d:%s: ", file, line, func);
+		vfprintf(the_debug_file, fmt, a);
 		va_end(a);
 	}
 }
